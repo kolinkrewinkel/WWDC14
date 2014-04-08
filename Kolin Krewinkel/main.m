608599d1651bd4a3deedae8b959a183778ce3bca
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2014-04-08 10:42:53 -0700

    Initial Commit

---
+++ Kolin Krewinkel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kolin Krewinkel
+//
+//  Created by Kolin Krewinkel on 4/8/14.
+//  Copyright (c) 2014 Kolin Krewinkel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KKRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KKRAppDelegate class]));
+    }
+}
